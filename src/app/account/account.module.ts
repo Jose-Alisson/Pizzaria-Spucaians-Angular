600926ce43883bd6f5a077b5ea3a8d4d6b0700ed
Jose-Alisson <84239112+Jose-Alisson@users.noreply.github.com>
2022-12-18 17:41:45 -0300

    pizzaria

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -4,6 +4,7 @@ import { AccountComponent } from './account.component';
 import { LoginComponent } from '../pages/account/login/login.component';
 import { SignUpComponent } from '../pages/account/sign-up/sign-up.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NgxMaskModule } from 'ngx-mask';
 
 const accountRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -17,7 +18,8 @@ const accountRoutes: Routes = [
     SignUpComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(accountRoutes)
+    RouterModule.forChild(accountRoutes),
+    NgxMaskModule.forRoot()
   ],
 })
 export class AccountModule {}
--- src/app/pages/account/sign-up/sign-up.component.ts
+++ src/app/pages/account/sign-up/sign-up.component.ts
@@ -3,13 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['../../../shared/styles/account.component.scss']
+  styleUrls: ['../../../shared/styles/account.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  count = 1;
+  textButton = 'Continuar';
+  step = 1;
+  maxStep = 3;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  voltar() {
+    if (this.step > 1) {
+      this.step--;
+      this.count--;
+    }
+
+    if (this.step == 4) {
+      this.textButton = 'Criar';
+    } else {
+      this.textButton = 'Continuar';
+    }
+
+    (<HTMLInputElement>document.getElementById('radio-' + this.count)).checked =
+      true;
   }
 
+  verificar() {
+    if (this.step < this.maxStep) {
+      this.step++;
+      this.count++;
+
+      if (this.step == 3) {
+        this.textButton = 'Criar';
+      } else {
+        this.textButton = 'Continuar';
+      }
+
+      if (this.count > 3) {
+        this.count = 1;
+      }
+
+      (<HTMLInputElement>(
+        document.getElementById('radio-' + this.count)
+      )).checked = true;
+    }
+  }
 }
